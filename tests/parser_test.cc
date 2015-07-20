38561244ce539df014763d0f6946c9ceb360551f
Ciro S. Costa <ciro9758@gmail.com>
2015-07-20 11:55:44 -0300

    improves tests - adds NestedMultipleAttributes

--- tests/parser_test.cc
+++ tests/parser_test.cc
@@ -93,7 +93,7 @@ TEST(Html, NestedWithSimblings) {
 }
 
 TEST(Html, FlatWithOneAttribute) {
-  bool debug = true;
+  bool debug = false;
   HTMLDriver driver(debug, debug);
   const char* source =
     "<body class=\"hue\">"
@@ -108,5 +108,49 @@ TEST(Html, FlatWithOneAttribute) {
   EXPECT_EQ(body->tag_name, "body");
   EXPECT_EQ(body->attr_map.size(), 1);
   EXPECT_EQ(body->attr_map["class"], "hue");
+}
+
+TEST(Html, NestedMultipleAttributes) {
+  bool debug = false;
+  HTMLDriver driver(debug, debug);
+  const char* source =
+    "<body class=\"hue\" id=\"cool\">"
+      "<tag class=\"hue\">"
+      "</tag>"
+      "<tag class=\"hue\">"
+        "huehue brbr"
+      "</tag>"
+    "</body>"
+    "";
+  driver.parse_source(source);
+
+  EXPECT_EQ(driver.dom.get()->type, NodeType::Element);
+
+  Element* body = dynamic_cast<Element*>(driver.dom.get());
+
+  EXPECT_EQ(body->tag_name, "body");
+
+  EXPECT_EQ(body->attr_map.size(), 2);
+  EXPECT_EQ(body->attr_map["class"], "hue");
+  EXPECT_EQ(body->attr_map["id"], "cool");
+
+  EXPECT_EQ(body->children.size(), 2);
+  EXPECT_EQ(body->children[0]->type, NodeType::Element);
+  EXPECT_EQ(body->children[1]->type, NodeType::Element);
+
+
+  Element* tag1 = dynamic_cast<Element*>(body->children[0].get());
+  Element* tag2 = dynamic_cast<Element*>(body->children[1].get());
+
+  EXPECT_EQ(tag1->attr_map.size(), 1);
+  EXPECT_EQ(tag2->attr_map.size(), 1);
+  EXPECT_EQ(tag1->attr_map["class"], "hue");
+  EXPECT_EQ(tag2->attr_map["class"], "hue");
+
+  EXPECT_EQ(tag2->children.front().get()->type, NodeType::Text);
+
+
+  Text* text = dynamic_cast<Text*>(tag2->children.front().get());
+  EXPECT_EQ(text->text, "huehue brbr");
 }
 
